f504b18ff7701b973b13a916f9dd0cf5f133e9d8
Isabelle Victoria <isabelle2015outlook@gmail.com>
2025-08-04 19:06:27 -0300

    Adicionar arquivos de projeto.

---
+++ organizador_de_documentos/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace organizador_de_documentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelecionar = new System.Windows.Forms.Button();
+            this.btnProcessar = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOrganizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelecionar
+            // 
+            this.btnSelecionar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(140)))), ((int)(((byte)(212)))));
+            this.btnSelecionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelecionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelecionar.ForeColor = System.Drawing.Color.White;
+            this.btnSelecionar.Location = new System.Drawing.Point(371, 240);
+            this.btnSelecionar.Name = "btnSelecionar";
+            this.btnSelecionar.Size = new System.Drawing.Size(96, 32);
+            this.btnSelecionar.TabIndex = 1;
+            this.btnSelecionar.Text = "Selecionar";
+            this.btnSelecionar.UseVisualStyleBackColor = false;
+            this.btnSelecionar.Click += new System.EventHandler(this.btnSelecionar_Click);
+            // 
+            // btnProcessar
+            // 
+            this.btnProcessar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(140)))), ((int)(((byte)(212)))));
+            this.btnProcessar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcessar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcessar.ForeColor = System.Drawing.Color.White;
+            this.btnProcessar.Location = new System.Drawing.Point(649, 240);
+            this.btnProcessar.Name = "btnProcessar";
+            this.btnProcessar.Size = new System.Drawing.Size(96, 32);
+            this.btnProcessar.TabIndex = 2;
+            this.btnProcessar.Text = "Processar";
+            this.btnProcessar.UseVisualStyleBackColor = false;
+            this.btnProcessar.Click += new System.EventHandler(this.btnProcessar_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLog.Location = new System.Drawing.Point(371, 360);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(397, 56);
+            this.txtLog.TabIndex = 4;
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvResultados.BackgroundColor = System.Drawing.Color.WhiteSmoke;
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Location = new System.Drawing.Point(371, 88);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.ReadOnly = true;
+            this.dgvResultados.RowHeadersVisible = false;
+            this.dgvResultados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvResultados.Size = new System.Drawing.Size(397, 136);
+            this.dgvResultados.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btnOrganizar
+            // 
+            this.btnOrganizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(119)))), ((int)(((byte)(140)))), ((int)(((byte)(212)))));
+            this.btnOrganizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrganizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrganizar.ForeColor = System.Drawing.Color.White;
+            this.btnOrganizar.Location = new System.Drawing.Point(649, 433);
+            this.btnOrganizar.Name = "btnOrganizar";
+            this.btnOrganizar.Size = new System.Drawing.Size(96, 32);
+            this.btnOrganizar.TabIndex = 7;
+            this.btnOrganizar.Text = "Organizar";
+            this.btnOrganizar.UseVisualStyleBackColor = false;
+            this.btnOrganizar.Click += new System.EventHandler(this.btnOrganizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(367, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Seus arquivos aparecerão aqui:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(367, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Arquivos processados:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.BackgroundImage = global::organizador_de_documentos.Properties.Resources.Seu_organizador_de_PDFs__agora_mais_inteligente_;
+            this.ClientSize = new System.Drawing.Size(799, 498);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOrganizar);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnProcessar);
+            this.Controls.Add(this.btnSelecionar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSelecionar;
+        private System.Windows.Forms.Button btnProcessar;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOrganizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ organizador_de_documentos/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UglyToad.PdfPig;
+using IronOcr;
+
+namespace organizador_de_documentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dgvResultados.Columns.Add("Arquivo", "Arquivo");
+            dgvResultados.Columns.Add("Categoria", "Categoria");
+            btnProcessar.Click += btnProcessar_Click;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnSelecionar_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Filter = "Arquivos PDF|*.pdf";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                    dgvResultados.Rows.Add(file, "Não processado");
+                }
+            }
+        }
+
+        private void btnProcessar_Click(object sender, EventArgs e)
+        {
+            txtLog.Clear();
+
+            foreach (DataGridViewRow row in dgvResultados.Rows)
+            {
+                string caminho = row.Cells[0].Value?.ToString();
+                if (string.IsNullOrWhiteSpace(caminho)) continue;
+
+                string texto = ExtrairTextoPdf(caminho);
+                if (string.IsNullOrWhiteSpace(texto))
+                {
+                    texto = ExtrairTextoViaOcr(caminho);
+                }
+
+                string categoria = DetectarCategoria(texto);
+                row.Cells[1].Value = categoria;
+                txtLog.AppendText($"[{categoria}] {Path.GetFileName(caminho)}\r\n");
+            }
+        }
+        private string ExtrairTextoPdf(string caminhoPdf)
+        {
+            try
+            {
+                using (var document = PdfDocument.Open(caminhoPdf))
+                {
+                    string textoCompleto = "";
+                    foreach (var page in document.GetPages())
+                    {
+                        textoCompleto += page.Text + "\n";
+                    }
+                    return textoCompleto;
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        private string ExtrairTextoViaOcr(string caminhoPdf)
+        {
+            try
+            {
+                var ocr = new IronTesseract();
+                var input = new OcrInput();
+                input.LoadPdf(caminhoPdf); 
+                var result = ocr.Read(input);
+                return result.Text;
+            }
+            catch (Exception ex)
+            {
+                return $"Erro ao executar OCR: {ex.Message}";
+            }
+        }
+
+
+        private string DetectarCategoria(string texto)
+        {
+            texto = texto.ToLower();
+            if (texto.Contains("boleto") || texto.Contains("vencimento"))
+                return "Boleto";
+            else if (texto.Contains("contrato") || texto.Contains("cláusula"))
+                return "Contrato";
+            else if (texto.Contains("certidão") || texto.Contains("registro"))
+                return "Certidão";
+            else
+                return "Outros";
+        }
+
+        private void btnOrganizar_Click(object sender, EventArgs e)
+        {
+            string pastaDocumentos = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string pastaBase = Path.Combine(pastaDocumentos, "PDFsOrganizados");
+
+            foreach (DataGridViewRow row in dgvResultados.Rows)
+            {
+                string caminho = row.Cells[0].Value?.ToString();
+                string categoria = row.Cells[1].Value?.ToString();
+
+                if (string.IsNullOrWhiteSpace(caminho) || string.IsNullOrWhiteSpace(categoria))
+                    continue;
+
+                try
+                {
+                    string destinoCategoria = Path.Combine(pastaBase, categoria);
+                    Directory.CreateDirectory(destinoCategoria);
+
+                    string nomeArquivo = Path.GetFileName(caminho);
+                    string destinoFinal = Path.Combine(destinoCategoria, nomeArquivo);
+
+                    // Verifica se o arquivo já existe
+                    if (File.Exists(destinoFinal))
+                    {
+                        DialogResult resposta = MessageBox.Show(
+                            $"O arquivo \"{nomeArquivo}\" já existe na categoria \"{categoria}\".\nDeseja substituir?",
+                            "Arquivo já existe",
+                            MessageBoxButtons.YesNoCancel,
+                            MessageBoxIcon.Warning);
+
+                        if (resposta == DialogResult.Cancel)
+                            break;
+                        else if (resposta == DialogResult.No)
+                            continue; // pula este arquivo
+                                      // se for Yes, continua e sobrescreve
+                    }
+
+                    File.Copy(caminho, destinoFinal, overwrite: true);
+                    txtLog.AppendText($"✔ {nomeArquivo} copiado para {destinoCategoria}\r\n");
+                }
+                catch (Exception ex)
+                {
+                    txtLog.AppendText($"Erro ao copiar {caminho}: {ex.Message}\r\n");
+                }
+            }
+
+            // Exibe mensagem e abre a pasta
+            MessageBox.Show("Arquivos organizados com sucesso!", "Pronto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            try
+            {
+                System.Diagnostics.Process.Start("explorer.exe", pastaBase);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível abrir a pasta: " + ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
